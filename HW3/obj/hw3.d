e48925fec6f01d7c46fa81b674e6b8a05b701848
Carter Steffen <cartste3@gmail.com>
2018-12-06 11:21:49 -0600

    eeprom read write and game data  '

--- HW3/obj/hw3.d
+++ HW3/obj/hw3.d
@@ -25,3 +25,5 @@
 .\obj\hw3.o: ..\peripherals\include\accel.h
 .\obj\hw3.o: ..\drivers\include\spi.h
 .\obj\hw3.o: ..\peripherals\include\spi_select.h
+.\obj\hw3.o: ..\peripherals\include\eeprom.h
+.\obj\hw3.o: ..\drivers\include\i2c.h
